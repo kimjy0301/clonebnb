ed293ebac1b2c954c2f41853304b83727745e4f0
kimjy0301 <znzld505@naver.com>
2019-10-23 16:10:15 +0900

    User 모델 수정

--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "영어"), (LANGUAGE_KOREAN, "한국어"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_CHOICES = ((CURRENCY_USD, "달러"), (CURRENCY_KRW, "원"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(
+        default=GENDER_MALE, choices=GENDER_CHOICES, max_length=10, blank=True
+    )
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(null=True)  # DateTImeField 랑은 다르다.
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
